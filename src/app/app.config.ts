c61ddb8dbc730f7724022fe5e4692f86e49e5384
Ekaterina <ekaterina@MacBook-Pro.local>
2023-11-01 22:38:45 +0900

    Initialize web application via create-cloudflare CLI
    Details:
      C3 = create-cloudflare@2.6.2
      project name = prisma-tidb-cloudflare
      framework = angular
      framework cli = @angular/cli@16.2.2
      package manager = npm@9.5.1
      wrangler = wrangler@3.15.0
      git = 2.39.2 (Apple Git-143)

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,10 @@
+import { provideClientHydration } from '@angular/platform-browser';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes) ]
+  providers: [provideHttpClient(withFetch()), provideClientHydration(), provideRouter(routes) ]
 };
